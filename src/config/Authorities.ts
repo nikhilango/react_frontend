2808a22712463d692bbab7e6f0bc1e7d0c6f2063
nikhilango <112620635+nikhilango@users.noreply.github.com>
2024-02-21 09:01:34 +0100

    initial commit

---
+++ src/config/Authorities.ts
@@ -0,0 +1,10 @@
+enum authorities {
+/*
+  add your authorities here. E.g:
+*/
+  USER_CREATE = "USER_CREATE",
+  USER_READ = "USER_READ",
+  USER_DEACTIVATE = "USER_DEACTIVATE",
+}
+
+export default authorities;
