dc722435e310e366eca47ed178051dccd8773f02
nikhilango <112620635+nikhilango@users.noreply.github.com>
2024-02-22 13:09:30 +0100

    corrected authorities

--- frontend/src/config/Authorities.ts
+++ frontend/src/config/Authorities.ts
@@ -2,9 +2,9 @@ enum authorities {
 /*
   add your authorities here. E.g:
 */
-  USER_CREATE = "USER_CREATE",
-  USER_READ = "USER_READ",
-  USER_DEACTIVATE = "USER_DEACTIVATE",
+  DEFAULT = "DEFAULT",
+  USER_MODIFY = "USER_MODIFY",
+  USER_DELETE = "USER_DELETE",
 }
 
 export default authorities;
