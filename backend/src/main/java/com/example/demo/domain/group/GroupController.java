89af37a55e0912e17202c8e9009b762aa04b2144
Henrikochh <113605484+Henrikochh@users.noreply.github.com>
2024-02-22 16:04:18 +0100

    Added new roles (GROUP_USER and NO_GROUP_USER) and added a new authority which allows you to see all groups.

--- backend/src/main/java/com/example/demo/domain/group/GroupController.java
+++ backend/src/main/java/com/example/demo/domain/group/GroupController.java
@@ -7,12 +7,14 @@ import com.example.demo.domain.user.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.UUID;
 
 @Validated
@@ -28,8 +30,14 @@ public class GroupController {
         this.groupMapper = groupMapper;
     }
     @GetMapping("/{id}")
-    public ResponseEntity<GroupDTO> oneGroupById(@PathVariable UUID id) {
+    public ResponseEntity<GroupDTO> groupById(@PathVariable UUID id) {
         Group group = groupService.getGroupById(id);
         return new ResponseEntity<>(groupMapper.toDTO(group), HttpStatus.OK);
+    }
+    @GetMapping({"", "/"})
+    @PreAuthorize("hasAuthority('USER_READ_GROUPS')")
+    public ResponseEntity<List<GroupDTO>> allGroups(){
+        List<Group> groups = groupService.getAllgroups();
+        return new ResponseEntity<>(groupMapper.toDTOs(groups), HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/example/demo/domain/group/GroupService.java
+++ backend/src/main/java/com/example/demo/domain/group/GroupService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.webjars.NotFoundException;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -19,5 +20,10 @@ public class GroupService {
     public Group getGroupById(UUID id){
     log.info("found id:" + id);
     return repository.findById(id).orElseThrow(() -> new NotFoundException("ID " + id + "not found"));
+    }
+
+    public List<Group> getAllgroups(){
+    log.info("found all id's");
+    return repository.findAll();
     }
 }
