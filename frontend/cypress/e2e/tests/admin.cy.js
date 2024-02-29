56b30710794621e6798c524e426381056bc3dcfe
f171p <filip.franek@noseryoung.com>
2024-02-29 09:47:16 +0100

    changed test case

--- frontend/cypress/e2e/tests/admin.cy.js
+++ frontend/cypress/e2e/tests/admin.cy.js
@@ -2,21 +2,24 @@
 import admin from '../../fixtures/admin.json'
 
  beforeEach(() => {
- cy.visit(admin.loginpage);
+    cy.visit(admin.loginpage);
 
- cy.get("#email").type(admin.email);
- cy.get("#password").type(admin.password);
- cy.contains("Sign in").click();
+    cy.get("#email").type(admin.email);
+    cy.get("#password").type(admin.password);
+    cy.contains("Sign in").click();
 
- cy.url().should("include", admin.homepage);
+    cy.url().should("include", admin.homepage);
  })
 
-describe('Admin Login', () => {
-    it('Navigates Admin to Homepage', () => {
+describe('User Edit', () => {
+    it('Admin Navigates to User List and Edits a User', () => {
        
         cy.contains("10 Users").click();
         cy.contains("See All Users").click();
 
         cy.url().should("include", admin.userspage);
+        cy.contains("Edit").click();
+        
+        cy.get("#Firstname").type("test");
     })
 })
