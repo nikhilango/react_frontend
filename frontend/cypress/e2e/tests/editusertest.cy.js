3116b93d752e62fe810334400c5ec5bde47bef89
f171p <filip.franek@noseryoung.com>
2024-02-29 13:30:28 +0100

    created test for editing users

---
+++ frontend/cypress/e2e/tests/editusertest.cy.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+import admin from '../../fixtures/admin.json'
+
+ beforeEach(() => {
+    cy.visit(admin.loginpage);
+
+    cy.get("#email").type(admin.email);
+    cy.get("#password").type(admin.password);
+    // cy.screenshot();
+    cy.contains("Sign in").click();
+
+    cy.url().should("include", admin.homepage);
+ })
+
+describe('Edit User', () => {
+    it('Admin Navigates to User List and Edits a User', () => {
+       
+        cy.contains("See All Users").click();
+        cy.url().should("include", admin.userspage);
+    //    cy.screenshot();
+        cy.contains("Edit").click();
+        
+        cy.get("#firstName").type("test");
+        cy.get("#lastName").type("test");
+    //    cy.screenshot();
+
+        cy.contains("Save").click();
+
+        cy.contains("Jamestest").should('be.visible');
+    //    cy.screenshot();
+    })
+})
+
